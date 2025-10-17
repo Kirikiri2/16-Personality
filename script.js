187e0c0644034a809627b66adbc93c1c85da71b3
Kristina <worklisa271@gmail.com>
2025-10-17 21:34:17 +0300

    homework-sass

--- script.js
+++
@@ -1,8 +0,0 @@
-const cardDiplomat = document.getElementById('Diplomat')
-const cardAnalysts = document.getElementById('Analysts')
-const cardExplorers = document.getElementById('Explorers')
-const cardSentinels = document.getElementById('Sentinels')
-
-cardDiplomat.addEventListener('click', () => {
-    document.
-})
