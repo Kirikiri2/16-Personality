fc4d1a672459aac0ccad2369cc7870b6f3ef905a
Kristina <worklisa271@gmail.com>
2025-10-17 19:21:06 +0300

    homework_1-3

---
+++ script.js
@@ -0,0 +1,8 @@
+const cardDiplomat = document.getElementById('Diplomat')
+const cardAnalysts = document.getElementById('Analysts')
+const cardExplorers = document.getElementById('Explorers')
+const cardSentinels = document.getElementById('Sentinels')
+
+cardDiplomat.addEventListener('click', () => {
+    document.
+})
